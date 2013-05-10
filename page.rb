ba9aa51c11895910d2660d9f4e842c0dfd338be0
Tim Mahoney <tim.mahoney@me.com>
2013-05-09 17:55:02 -0700

    Now allowing more customization of loading the HTML.

--- page.rb
+++ page.rb
@@ -8,12 +8,34 @@ class Page
   end
 
   def load(&callback)
-    url = "#{page_name}_page.html"
-    load_html(url) { |success| callback.call(success) }
+    if @element
+      callback.call(@element)
+      return
+    end
+
+    load_html do |element|
+      @element = element
+      did_load
+      callback.call(element)
+    end
   end
 
 
   protected
+
+  # Loads the HTML for the page.
+  # When the loading is finished, this calls the callback
+  # with the loaded element as the argument.
+  # If the argument is nil, then no element was loaded.
+  # 
+  # By default, the HTML will be loaded from #{page_name}_page.html.
+  # Subclasses can override this function to load the HTML manually.
+  def load_html(&callback)
+    url = "#{@page_name}_page.html"
+    load_html_internal(url) do |element|
+      callback.call(element)
+    end
+  end
 
   # This function is called when the element for the page is loaded.
   # When this function is called, the @element variable from this class
@@ -24,9 +46,7 @@ class Page
 
   private
 
-  # Loads the HTML for a page into the element of this controller.
-  # This acts as setting the view for this controller.
-  def load_html(url, &callback)
+  def load_html_internal(url, &callback)
     request = XMLHttpRequest.new
     request.open('GET', url)
     request.response_type = 'document'
@@ -35,16 +55,14 @@ class Page
 
       response = request.response
       if !response
-        callback.call(false)
+        callback.call(nil)
         next
       end
 
-      @element = $window.document.create_element('div')
-      @element.class_list.add("#{@page_name}-page")
-      response.body.children.entries.each { |child| @element.append_child(child) }
-
-      did_load
-      callback.call(true)
+      element = $window.document.create_element('div')
+      element.class_list.add("#{@page_name}-page")
+      response.body.children.entries.each { |child| element.append_child(child) }
+      callback.call(element)
     end
 
     request.send()
