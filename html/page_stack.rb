1ba4556e48f8c71578fa1032771294f3f1a5a578
Tim Mahoney <tim.mahoney@me.com>
2013-05-21 17:45:22 -0700

    Added some animation between pages.

--- html/page_stack.rb
+++ html/page_stack.rb
@@ -3,6 +3,9 @@ require 'loading_screen.rb',
 
 class PageStack < Page
 
+  PAGE_AFTER_HIDE_STYLE = 'after-hide-position'
+  PAGE_BEFORE_SHOW_STYLE = 'before-show-position'
+
   def initialize
     super('page-stack')
     @stack = []
@@ -20,7 +23,7 @@ class PageStack < Page
 
     $window.history.push_state(state_object_for_page(page), nil, page.location_bar_url)
 
-    load_and_show_page(page)
+    load_and_show_page(page, animated)
   end
 
   def update_location_bar_url
@@ -54,7 +57,7 @@ class PageStack < Page
     }
   end
 
-  def load_and_show_page(page)
+  def load_and_show_page(page, animated)
     loading_screen = LoadingScreen.new
     loading_screen.show
 
@@ -62,23 +65,46 @@ class PageStack < Page
       loading_screen.hide
 
       if page.element
-        hide_page(@current_page) if @current_page
+        hide(page: @current_page, animated: animated) if @current_page
         @current_page = page
-        show_page(@current_page)
+        show(page: @current_page, animated: animated)
       end
     end
   end
 
-  def show_page(page)
+  def show(page: nil, animated: true, style: 'page-stack-before-show')
+    return if !page
+
     page.will_appear
+    page.element.class_list.add('page-stack-transition', 'before-show-transparency', style)
     @element.append_child(page.element)
-    page.did_appear
+
+    $window.set_timeout(0) do 
+      page.element.class_list.remove(style, 'before-show-transparency')
+
+      $window.set_timeout(300) do
+        page.element.class_list.remove('page-stack-transition')
+        page.did_appear
+      end
+    end    
   end
 
-  def hide_page(page)
+  def hide(page: nil, style: PAGE_AFTER_HIDE_STYLE, animated: true)
+    return if !page
+
     page.will_disappear
-    @element.remove_child(page.element)
-    page.did_disappear
+
+    if animated
+      page.element.class_list.add('page-stack-transition', 'after-hide-transparency', style)
+      $window.set_timeout(300) do
+        @element.remove_child(page.element)
+        page.element.class_list.remove('page-stack-transition', 'after-hide-transparency', style)
+        page.did_disappear
+      end
+    else
+      @element.remove_child(page.element)
+      page.did_disappear
+    end
   end
 
   def on_pop_state(event)
@@ -89,9 +115,16 @@ class PageStack < Page
     page_to_show = @id_to_page[event.state[:page_id]]
     return if !page_to_show
 
-    hide_page(@current_page) if @current_page
+    page_index = @stack.index(page_to_show)
+    previous_index = @stack.index(@current_page) || 0
+    $window.console.log("Page index: #{page_index} previous=#{previous_index}")
+    going_forward = (page_index > previous_index)
+    previous_page_new_style = going_forward ? PAGE_AFTER_HIDE_STYLE : PAGE_BEFORE_SHOW_STYLE
+    next_page_new_style = going_forward ? PAGE_BEFORE_SHOW_STYLE : PAGE_AFTER_HIDE_STYLE
+
+    hide(page: @current_page, style: previous_page_new_style)
     @current_page = page_to_show
-    show_page(@current_page)
+    show(page: @current_page, style: next_page_new_style)
   end
 
 end # PageStack
