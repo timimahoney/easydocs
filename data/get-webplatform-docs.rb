283bfff63a87c643f9ea2d7bcbe849f014cc3792
Tim Mahoney <tim.mahoney@me.com>
2013-05-27 16:11:46 -0700

    Added a little comment to this script.

--- data/get-webplatform-docs.rb
+++ data/get-webplatform-docs.rb
@@ -1,3 +1,9 @@
+# This script will request and save the documentation needed from the WebPlatform API.
+# Run it with 'ruby get-webplatform-docs.rb /path/to/output-directory'.
+# It will output a few JSON files and one 'interfaces.xml' file in the output-directory.
+# By default, the output directory is the current directory.
+# The 'interfaces.xml' file is the one that is read by EasyDocs.
+
 require './web-platform-docs.rb'
 require 'nokogiri'
 
@@ -6,7 +12,6 @@ output_dir = ARGV[0] || './'
 
 request_and_write_all(output_dir)
 interfaces = load_interfaces_with_attached_members(output_dir)
-# check_for_duplicates(interfaces)
 builder = build_xml(interfaces)
 
 p 'Outputting XML...'
