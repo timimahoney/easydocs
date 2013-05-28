db8d54c745e31d115aeb34d41742f0e63fb5af53
Tim Mahoney <tim.mahoney@me.com>
2013-05-27 17:04:23 -0700

    These listeners

--- site/script/index.rb
+++ site/script/index.rb
@@ -4,4 +4,8 @@ window.add_event_listener 'load' do
   WebDocs.start
 end
 
+# FIXME: Remove these listeners.
+window.add_event_listener('unload') { $window.console.log('unload') }
+window.add_event_listener('beforeunload') { $window.console.log('beforeunload') }
+
 end # require
