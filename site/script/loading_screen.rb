b171c7e900b5ddb86e6f0a4dbc3d39f1520ea474
Tim Mahoney <tim.mahoney@me.com>
2013-05-28 18:37:48 -0700

    Removed the spin.js stuff.

--- site/script/loading_screen.rb
+++ site/script/loading_screen.rb
@@ -1,5 +1,3 @@
-require 'spin.min.js' do
-
 class LoadingScreen
   attr_reader :element
 
@@ -19,33 +17,6 @@ class LoadingScreen
     bar = $window.document.create_element('div')
     bar.class_list.add('loading-bar')
     container.append_child(bar)
-  
-    add_spinner_script = $window.document.create_element('script')
-    add_spinner_script.type = 'text/javascript'
-    script = <<EOF
-  var options = {
-    lines: 8, // The number of lines to draw
-    length: 0, // The length of each line
-    width: 4, // The line thickness
-    radius: 10, // The radius of the inner circle
-    corners: 1, // Corner roundness (0..1)
-    rotate: 90, // The rotation offset
-    direction: 1, // 1: clockwise, -1: counterclockwise
-    color: '#fff', // #rgb or #rrggbb
-    speed: 1, // Rounds per second
-    trail: 50, // Afterglow percentage
-    shadow: false, // Whether to render a shadow
-    hwaccel: false, // Whether to use hardware acceleration
-    className: 'spinner', // The CSS class to assign to the spinner
-    zIndex: 2e9, // The z-index (defaults to 2000000000),
-    top: '10px',
-    left: '10px'
-  };
-  var target = document.getElementById('#{@element.id}');
-  var spinner = new Spinner(options).spin(target);
-EOF
-    add_spinner_script.inner_html = script
-    $window.document.head.append_child(add_spinner_script);
   end
 
   def show
@@ -68,5 +39,3 @@ EOF
   end
 
 end # LoadingScreen
-
-end # require
