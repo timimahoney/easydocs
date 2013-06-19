5144096b87b615de168c89dffa13cdbd6766ab68
Tim Mahoney <tim.mahoney@me.com>
2013-06-19 08:58:15 -0700

    This is somewhat like a static function.

--- site/scripts/class-page.js
+++ site/scripts/class-page.js
@@ -57,7 +57,7 @@ Object.defineProperty(ClassPage.prototype, 'currentMember', {
   }
 });
 
-ClassPage.prototype.isValidUrl = function(url) {
+ClassPage.isValidUrl = function(url) {
   if (!url) {
     return false;
   }
