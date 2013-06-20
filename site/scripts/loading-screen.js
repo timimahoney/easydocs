20f097ef6ddd8ee26f8dd5473c4c67faf0135f64
Tim Mahoney <tim.mahoney@me.com>
2013-06-20 09:39:24 -0700

    Added the loading screen.

---
+++ site/scripts/loading-screen.js
@@ -0,0 +1,52 @@
+var LoadingScreen = {
+  MINIMUM_SHOW_TIME: 1000,
+
+  show: function() {
+    if (!this._element) {
+      this._initializeElement();
+    }
+
+    this._canHideAt = Date.now() + this.MINIMUM_SHOW_TIME;
+    document.body.appendChild(this._element);
+    window.setTimeout(function() {
+      this._element.classList.remove('hidden');
+    }.bind(this), 0);
+  },
+
+  hide: function() {
+    if (!this._canHideAt || !this._element) {
+      return;
+    }
+
+    var timeLeftUntilHide = this._canHideAt - Date.now();
+    if (timeLeftUntilHide > 0) {
+      window.setTimeout(function() {
+        this.hide();
+      }.bind(this), timeLeftUntilHide);
+
+      return;
+    }
+
+    this._element.classList.add('hidden');
+    window.setTimeout(function() {
+      this._element.parentNode.removeChild(this._element);
+    }.bind(this), 400);
+  },
+
+  _initializeElement: function() {
+    this._element = document.createElement('div');
+    this._element.classList.add('loading-screen', 'hidden', 'transition');
+
+    var container = document.createElement('div');
+    container.classList.add('loading-container');
+    this._element.appendChild(container);
+
+    text = document.createElement('p');
+    text.innerText = 'Loading data...';
+    container.appendChild(text);
+
+    bar = document.createElement('div');
+    bar.classList.add('loading-bar');
+    container.appendChild(bar);
+  }
+};
--- site/scripts/web-docs.js
+++ site/scripts/web-docs.js
@@ -4,15 +4,19 @@ window.WebDocs = {
   pageStack: new PageStack(),
 
   start: function() {
+    LoadingScreen.show();
+
     InterfaceDatabase.loadInterfaces(function() {
 
       WebDocs.pageStack.load(function() {
-        document.body.appendChild(WebDocs.pageStack.element);
+        document.body.insertBefore(WebDocs.pageStack.element, document.body.firstElementChild);
 
         var path = window.location.pathname;
         path = path.substr(1);
         var page = URLHandler.pageForUrl(path);
         WebDocs.pageStack.push(page, false);
+
+        LoadingScreen.hide();
       });
 
     });
