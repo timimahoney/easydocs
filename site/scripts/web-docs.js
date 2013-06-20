4be0eb32acbc6e51f6f42adfe5e6906f754c2ca2
Tim Mahoney <tim.mahoney@me.com>
2013-06-20 12:32:00 -0700

    Not showing the loading screen for short loads.

--- site/scripts/web-docs.js
+++ site/scripts/web-docs.js
@@ -2,9 +2,15 @@ window.WebDocs = {
   pageStack: new PageStack(),
 
   start: function() {
-    LoadingScreen.show();
+
+    // Wait a bit before showing the loading screen.
+    // That way, it won't show for very short loads.
+    var loadingTimeoutId = window.setTimeout(function () {
+      LoadingScreen.show();
+    }, 100);
 
     InterfaceDatabase.loadInterfaces(function() {
+      window.clearTimeout(loadingTimeoutId);
 
       WebDocs.pageStack.load(function() {
         document.body.insertBefore(WebDocs.pageStack.element, document.body.firstElementChild);
